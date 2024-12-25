5b19d77f6dbc2e299229e5a6a63a68ec193e4ebd
Yuh JooKyung <95693039+JooKyung-Yuh@users.noreply.github.com>
2024-12-25 10:45:41 +0900

    session null 타입도 포함

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,6 +5,17 @@ import { NewMemoCard } from '@/components/NewMemoCard'
 import { MemoList } from '@/components/MemoList'
 import { Search } from '@/components/ui/search'
 
+interface User {
+  id: string
+  name?: string | null
+  email?: string | null
+  image?: string | null
+}
+
+interface Session {
+  user: User | null
+}
+
 async function getInitialMemos(userId: string, query?: string) {
   const limit = 9
   const memos = await prisma.memo.findMany({
@@ -32,7 +43,7 @@ interface PageProps {
 }
 
 export default async function DashboardPage({ searchParams }: PageProps) {
-  const session = await getServerSession(authOptions)
+  const session = (await getServerSession(authOptions)) as Session | null
   const memos = session?.user?.id
     ? await getInitialMemos(session.user.id, searchParams.q)
     : []
